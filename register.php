9d28530acbf92917494868b9b17a269898ae122b
thongnim133 <thongnim133@gmail.com>
2022-07-10 00:22:50 -0700

    lol

--- register.php
+++ register.php
@@ -20,27 +20,43 @@
     ?>
 
     <div class="container">
-        <h1> สมัครสมาชิก </h1>
-       
 
-        <form action="reg_insert.php" method="POST">
-            Username:
-            <input type="text" name="username" class="form-control" />
-            Password:
-            <input type="text" name="password" class="form-control" />
-            ชื่อ:
-            <input type="text" name="firstname" class="form-control" />
-            นามสกุล:
-            <input type="text" name="lastname" class="form-control" />
-            email:
-            <input type="text" name="email" class="form-control" />
-            ถิ่นที่อยู่:
-            <textarea name="address" row="4" class="form-control"></textarea>
-            <input type="submit" value="บันทึกข้อมูล" class="btn btn-primary"/>
+        <div class="row justify-content-center">
 
-        </form>
- <a href="./index.php"> ยุ่งยากเกินไป...กลับหน้าบ้าน </a>
+            <div class="col-md-6">
+                <h1 style="color:red" class="text-center"> สมัครสมาชิก </h1>
+
+
+                <form action="reg_insert.php" method="POST">
+                    Username:
+                    <input type="text" name="username" class="form-control" />
+                    Password:
+                    <input type="text" name="password" class="form-control" />
+                    ชื่อ:
+                    <input type="text" name="firstname" class="form-control" />
+                    นามสกุล:
+                    <input type="text" name="lastname" class="form-control" />
+                    email:
+                    <input type="text" name="email" class="form-control" />
+                    ถิ่นที่อยู่:
+
+
+                    <textarea name="address" row="4" class="form-control"></textarea>
+                    <div class="pt-2 d-grid">
+                        <input type="submit" value="บันทึกข้อมูล" class="btn btn-primary" />
+                    </div>
+                </form>
+                <div class="text-center">
+                    <br />
+                    <a href="./index.php"> ยุ่งยากเกินไป...กลับหน้าบ้าน </a>
+                    <br />
+                    <img src="./image/ลุงแซลมอน.png" height=250px>
+                </div>
+            </div>
+        </div>
     </div>
+    </div>
+
 
 </body>
 
